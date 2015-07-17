cd89e93a84489ab43926fae36092ad64aaf1a390
masternone <themasternone@gmail.com>
2015-07-17 12:32:54 -0400

    Refactoring

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,5 @@
-;(function(angular) {
+;
+(function(angular){
 
     'use strict';
 
@@ -8,8 +9,22 @@
     angular.module('cakeOrderForm', ['ngRoute', 'ngMessages', 'ngAnimate', 'ngMaterial'])
         .config(['$routeProvider', '$locationProvider', AppConfig])
         .controller('AppCtrl', ['$route', '$routeParams', '$location', AppCtrl])
-        .controller('OrderFormCtrl', ['OrderFormData','StateFactory', 'CakeFlavors', 'FrostingFlavors', 'LayerSizes', OrderFormCtrl])
-        .controller('PrintCtrl', ['OrderFormData', 'StateFactory', 'CakeFlavors', 'FrostingFlavors', 'LayerSizes', PrintCtrl])
+        .controller('OrderFormCtrl', [
+            'OrderFormData',
+            'StateFactory',
+            'CakeFlavors',
+            'FrostingFlavors',
+            'LayerSizes',
+            OrderFormCtrl
+        ])
+        .controller('PrintCtrl', [
+            'OrderFormData',
+            'StateFactory',
+            'CakeFlavors',
+            'FrostingFlavors',
+            'LayerSizes',
+            PrintCtrl
+        ])
         .factory('OrderFormData', OrderFormData)
         .factory('StateFactory', ['$http', StateFactory])
         .factory('CakeFlavors', CakeFlavors)
@@ -18,7 +33,7 @@
         .directive('phoneValid', phoneValid)
         .directive('zipcodeValid', zipcodeValid)
         .directive('payPalIcon', PayPalIcon)
-        ;
+    ;
 
     function PayPalIcon(){
         return {
@@ -30,7 +45,7 @@
     function zipcodeValid(){
         return {
             require: 'ngModel',
-            link: function(scope, elm, attrs, ctrl) {
+            link: function(scope, elm, attrs, ctrl){
                 ctrl.$validators.zipcode = function(modelValue, viewValue){
                     if(ctrl.$isEmpty(modelValue)){
                         // consider empty models to be valid
@@ -54,7 +69,7 @@
             require: 'ngModel',
             link: function(scope, elm, attrs, ctrl){
                 ctrl.$validators.phoneNum = function(modelValue, viewValue){
-                    if (ctrl.$isEmpty(modelValue)){
+                    if(ctrl.$isEmpty(modelValue)){
                         // consider empty models to be valid
                         return true;
                     }
@@ -72,7 +87,7 @@
     }
 
     function LayerSizes(){
-        var LayerSizes = {};
+        var LayerSizes      = {};
         LayerSizes.getSizes = function(){
             return [
                 {"name": "6 inch round", "cost": 10},
@@ -84,58 +99,72 @@
                 {"name": "1/8 sheet", "cost": 10},
                 {"name": "1/4 sheet", "cost": 15},
                 {"name": "1/2 sheet", "cost": 20},
-                {"name": "full sheet", "cost": 40},
+                {"name": "full sheet", "cost": 40}
             ]
-        }
+        };
         return LayerSizes;
     }
 
     function CakeFlavors(){
-        var CakeFlavors = {};
+        var CakeFlavors        = {};
         CakeFlavors.getFlavors = function(){
             return [
-                {"name": 'Choclate', "cost": 0},
-                {"name": 'German Cholacte', "cost": 0},
+                {"name": 'Chocolate', "cost": 0},
+                {"name": 'German Chocolate', "cost": 0},
                 {"name": 'Red Velvet', "cost": 0},
                 {"name": 'Yellow', "cost": 0},
                 {"name": 'White', "cost": 0},
                 {"name": 'Spice', "cost": 0},
                 {"name": 'Carrot', "cost": 0}
             ]
-        }
+        };
         return CakeFlavors;
     }
-    
+
     function FrostingFlavors(){
-        var FrostingFlavors = {};
+        var FrostingFlavors        = {};
         FrostingFlavors.getFlavors = function(){
             return [
                 {"name": 'Buttercream', "cost": 0},
-                {"name": 'Choclate Buttercream', "cost": 0},
-                {"name": 'Wipped', "cost": 0},
-                {"name": 'Choclate Wipped', "cost": 0},
-                {"name": 'Creamcheese', "cost": 0}
+                {"name": 'Chocolate Buttercream', "cost": 0},
+                {"name": 'Whipped', "cost": 0},
+                {"name": 'Chocolate Whipped', "cost": 0},
+                {"name": "Cream Cheese", "cost": 0}
             ]
-        }
+        };
         return FrostingFlavors;
     }
-    
+
     function StateFactory($http){
-        var StateFactory = {}
+        var StateFactory       = {};
         StateFactory.getStates = function(){
             return $http.get('./js/data/States.json')
-        }
+        };
         return StateFactory;
     }
 
     function OrderFormData(){
-        var data = {};
+        var trackedItems       = [
+                'firstName',
+                'lastName',
+                'email',
+                'phoneNum',
+                'addressOne',
+                'addressTwo',
+                'city',
+                'state',
+                'zipcode',
+                'payment',
+                'layers'
+            ],
+            trackedItemsLength = trackedItems.length,
+            data               = {};
 
         // This is only being used to speed up testing
         data.firstName  = 'Edward';
         data.lastName   = 'Grant';
         data.email      = 'themasternone@gmail.com';
-        data.phoneNum   = '313.799.2101'
+        data.phoneNum          = '313.799.2101';
         data.addressOne = '18441 Delaware Ave';
         data.addressTwo = undefined;
         data.city       = 'RedFord';
@@ -144,57 +173,32 @@
         data.payment    = 'PayPal';
         data.layers     = [];
 
-        var OrderFormData = {
-            getFirstName  : function(){ return data.firstName; },
-            setFirstName  : function(firstName){ data.firstName = firstName; },
-            getLastName   : function(){ return data.lastName; },
-            setLastName   : function(lastName){ data.lastName = lastName; },
-            getEmail      : function(){ return data.email; },
-            setEmail      : function(email){ data.email = email; },
-            getPhoneNum   : function(){ return data.phoneNum; },
-            setPhoneNum   : function(phoneNum){ data.phoneNum = phoneNum; },
-            getAddressOne : function(){ return data.addressOne; },
-            setAddressOne : function(addressOne){ data.addressOne = addressOne; },
-            getAddressTwo : function(){ return data.addressTwo; },
-            setAddressTwo : function(addressTwo){ data.addressTwo = addressTwo; },
-            getCity       : function(){ return data.city; },
-            setCity       : function(city){ data.city = city; },
-            getState      : function(){ return data.state; },
-            setState      : function(state){ data.state = state; },
-            getZipcode    : function(){ return data.zipcode; },
-            setZipcode    : function(zipcode){ data.zipcode = zipcode; },
-            getPayment    : function(){ return data.payment; },
-            setPayment    : function(payment){ data.payment = payment; },
-            getLayers     : function(){ return data.layers || []; },
-            setLayers     : function(layers){ data.layers = layers; },
-
-            apply         : function(self){
-                self.firstName  = OrderFormData.getFirstName();
-                self.lastName   = OrderFormData.getLastName();
-                self.email      = OrderFormData.getEmail();
-                self.phoneNum   = OrderFormData.getPhoneNum();
-                self.addressOne = OrderFormData.getAddressOne();
-                self.addressTwo = OrderFormData.getAddressTwo();
-                self.city       = OrderFormData.getCity();
-                self.state      = OrderFormData.getState();
-                self.zipcode    = OrderFormData.getZipcode();
-                self.payment    = OrderFormData.getPayment();
-                self.layers     = OrderFormData.getLayers();
+        var OrderFormData;
+        OrderFormData = {
+            save : function(source){
+                for(var i = 0; i < trackedItemsLength; i++){
+                    data[trackedItems[i]] = source[trackedItems[i]]
+                }
+            },
+            apply: function(self){
+                for(var i = 0; i < trackedItemsLength; i++){
+                    self[trackedItems[i]] = data[trackedItems[i]]
+                }
             }
-        }
+        };
         return OrderFormData;
     }
 
-    function AppConfig($routeProvider, $locationProvider) {
+    function AppConfig($routeProvider, $locationProvider){
         $routeProvider
             .when('/', {
                 templateUrl: './partials/form.html',
-                controller: 'OrderFormCtrl',
+                controller : 'OrderFormCtrl',
                 controllerAs: 'order'
             })
             .when('/print', {
                 templateUrl: './partials/print.html',
-                controller: 'PrintCtrl',
+                controller : 'PrintCtrl',
                 controllerAs: 'print'
             })
             .otherwise({
@@ -204,69 +208,59 @@
         //$locationProvider.html5Mode(true);
     }
 
-    function AppCtrl($route, $routeParams, $location, $mdThemingProvider) {
+    function AppCtrl($route, $routeParams, $location){
         //this.$route = $route;
         //this.$location = $location;
         //this.$routeParams = $routeParams;
     }
 
-    function OrderFormCtrl(OrderFormData, StateFactory, CakeFlavors, FrostingFlavors, LayerSizes) {
+    function OrderFormCtrl(OrderFormData, StateFactory, CakeFlavors, FrostingFlavors, LayerSizes){
         //this.params = $routeParams;
-        this.layers = [];
+        this.layers      = [];
         this.cakeFlavors = CakeFlavors.getFlavors();
         this.frostingFlavors = FrostingFlavors.getFlavors();
-        this.layerSizes = LayerSizes.getSizes();
-        var self = this;
+        this.layerSizes  = LayerSizes.getSizes();
+        var self         = this;
         StateFactory.getStates()
             .success(function(States){
-                self.states = States;
-                if(!self.state && self.state !== 0 )
-                    self.state = 26;
-            });
-        this.setFirstName  = function(){ OrderFormData.setFirstName(this.firstName); }
-        this.setLastName   = function(){ OrderFormData.setLastName(this.lastName); }
-        this.setEmail      = function(){ OrderFormData.setEmail(this.email); }
-        this.setPhoneNum   = function(){ OrderFormData.setPhoneNum(this.phoneNum); }
-        this.setAddressOne = function(){ OrderFormData.setAddressOne(this.addressOne); }
-        this.setAddressTwo = function(){ OrderFormData.setAddressTwo(this.addressTwo); }
-        this.setCity       = function(){ OrderFormData.setCity(this.city); }
-        this.setState      = function(){ OrderFormData.setState(this.state); }
-        this.setZipcode    = function(){ OrderFormData.setZipcode(this.zipcode); }
-        this.setPayment    = function(){ OrderFormData.setPayment(this.payment); }
-        this.setLayers     = function(){ OrderFormData.setLayers(this.layers); }
-        this.addLayer      = function(){
-            console.log('inside add layer');
-            console.log('this', this);
-            console.log('self', self);
+                                 self.states = States;
+                                 if(!self.state && self.state !== 0)
+                                     self.state = 26;
+                             });
+
+        this.save     = function(){
+            OrderFormData.save(this);
+        };
+        this.addLayer = function(){
             this.layers.push({
-                cake     : '',
-                frosting : '',
-                size     : ''
+                cake    : '',
+                frosting: '',
+                size    : ''
             });
-        }
+        };
         OrderFormData.apply(this);
     }
 
-    function PrintCtrl(OrderFormData, StateFactory, CakeFlavors, FrostingFlavors, LayerSizes) {
+    function PrintCtrl(OrderFormData, StateFactory, CakeFlavors, FrostingFlavors, LayerSizes){
         this.cakeFlavors = CakeFlavors.getFlavors();
         this.frostingFlavors = FrostingFlavors.getFlavors();
-        this.layerSizes = LayerSizes.getSizes();
-        this.total = 0;
-        this.totalup = function(){
-            for(layer in this.layers){
+        this.layerSizes  = LayerSizes.getSizes();
+        this.total       = 0;
+        this.totalup     = function(){
+            for(var layer in this.layers){
                 console.log(layer);
             }
-        }
-        this.isPayPal = function(){
+        };
+        this.isPayPal    = function(){
             return this.payment == 'PayPal';
-        }
-        var self = this;
+        };
+        var self         = this;
         StateFactory.getStates()
             .success(function(States){
-                self.states = States;
-                if(!self.state && self.state !== 0 )
-                    self.state = 26;
-            });
+                                 self.states = States;
+                                 if(!self.state && self.state !== 0)
+                                     self.state = 26;
+                             });
         OrderFormData.apply(this);
     }
 })(window.angular);
