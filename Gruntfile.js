8b379e6b84fd4e0f047a2379d38092387335351e
masternone <themasternone@gmail.com>
2015-08-13 10:51:27 -0400

    Added PostCSS and some adjustments for small screens

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,78 @@
-module.exports = function(grunt) {
+module.exports = function(grunt){
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			combinedCSS: {
-				files: {
-					'./css/less/combined.less': [
-						'./css/less/source/_*.less',
-						'./css/less/source/_*.css'
-					]
-				}
-			},
-		},
-		less: {
-			app: {
-				files: {
-					'./css/app.css': [
-						'./css/less/combined.less'
-					],
-				}
-			}
-		},
-		develop: {
-			server: {
-				file: 'app.js'
-			}
-		},
-		watch: {
-			// node: {
-			// 	files: [
-			// 		'./app.js',
-			// 		'./serverResponse/*'
-			// 	],
-			// 	tasks: ['develop'],
-			// 	options: { nospawn: true }
-			// },
-			combinedCSS: {
-				files: [
-					'./css/less/source/*'
-				],
-				tasks: ['concat:combinedCSS', 'less:app'],
-				options: {
-					spawn: false,
-					livereload: true,
-				},
-			},
-		},
-	});
+    // Project configuration.
+    grunt.initConfig({
+        pkg    : grunt.file.readJSON('package.json'),
+        concat : {
+            combinedCSS: {
+                files: {
+                    './css/less/combined.less': [
+                        './css/less/source/_*.less',
+                        './css/less/source/_*.css'
+                    ]
+                }
+            },
+        },
+        less   : {
+            app: {
+                files: {
+                    './css/app.css': [
+                        './css/less/combined.less'
+                    ],
+                }
+            }
+        },
+        postcss: {
+            options: {
+                processors: [
+                    require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+                    require('cssnano')() // minify the result
+                ]
+            },
+            dist   : {
+                src: 'css/*.css'
+            }
+        },
+        develop: {
+            server: {
+                file: 'http-server'
+            }
+        },
+        watch  : {
+            // node: {
+            // 	files: [
+            // 		'./app.js',
+            // 		'./serverResponse/*'
+            // 	],
+            // 	tasks: ['develop'],
+            // 	options: { nospawn: true }
+            // },
+            combinedCSS: {
+                files  : [
+                    './css/less/source/*'
+                ],
+                tasks  : ['concat:combinedCSS', 'less:app'],
+                options: {
+                    spawn     : false,
+                    livereload: true,
+                },
+            },
+        },
+    });
 
-	// Load the plugins
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-develop');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-develop');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// grunt.registerTask('devserver', ['concat', 'jst', 'less', 'develop', 'watch']);
-	grunt.registerTask('dev', ['concat', 'less']); // this one will not start a server
-	grunt.registerTask('server', ['develop']); // Starting the server
-	grunt.registerTask('auto', ['watch']); // Initiating the watch task cannot be called watch because that would cause an infinite loop
-	// Default task(s).
-	grunt.registerTask('default', ['concat', 'less']);
+    // grunt.registerTask('devserver', ['concat', 'jst', 'less', 'develop', 'watch']);
+    grunt.registerTask('dev', ['concat', 'less', 'postcss']); // this one will not start a server
+    grunt.registerTask('server', ['develop']); // Starting the server
+    grunt.registerTask('auto', ['watch']); // Initiating the watch task cannot be called watch because that would cause an infinite loop
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'less', 'postcss']);
 
 };
