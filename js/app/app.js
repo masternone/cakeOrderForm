0a3434691cd01f24a15a964902b1e9ed3a247f9f
masternone <themasternone@gmail.com>
2015-09-03 14:45:47 -0400

    Added in the Other option and added in an other layer option

--- js/app/app.js
+++ js/app/app.js
@@ -13,10 +13,17 @@
         .controller('PrintCtrl', ['OrderFormData', 'StateFactory', 'LayerElements', PrintCtrl])
         .factory('OrderFormData', OrderFormData)
         .factory('StateFactory', ['$http', StateFactory])
-        .factory('LayerElements', ['CakeFlavors', 'FrostingFlavors', 'LayerSizes', LayerElements])
+        .factory('LayerElements', [
+            'LayerSizes',
+            'CakeFlavors',
+            'FrostingFlavors',
+            'DecorationComplexity',
+            LayerElements
+        ])
         .factory('CakeFlavors', CakeFlavors)
         .factory('FrostingFlavors', FrostingFlavors)
         .factory('LayerSizes', LayerSizes)
+        .factory('DecorationComplexity', DecorationComplexity)
         .directive('phoneValid', phoneValid)
         .directive('zipcodeValid', zipcodeValid)
         .directive('payPalIcon', PayPalIcon)
@@ -73,14 +80,15 @@
         };
     }
 
-    function LayerElements(CakeFlavors, FrostingFlavors, LayerSizes){
-        var LayerElements             = {};
-        LayerElements.getElements     = function(){
-            return ['size', 'cake', 'frosting']
+    function LayerElements(LayerSizes, CakeFlavors, FrostingFlavors, DecorationComplexity){
+        var LayerElements                  = {};
+        LayerElements.getElements          = function(){
+            return ['size', 'cake', 'frosting', 'complexity']
         };
-        LayerElements.CakeFlavors     = CakeFlavors;
-        LayerElements.FrostingFlavors = FrostingFlavors;
-        LayerElements.LayerSizes      = LayerSizes;
+        LayerElements.LayerSizes           = LayerSizes;
+        LayerElements.CakeFlavors          = CakeFlavors;
+        LayerElements.FrostingFlavors      = FrostingFlavors;
+        LayerElements.DecorationComplexity = DecorationComplexity;
         return LayerElements;
     }
 
@@ -97,7 +105,8 @@
                 {"id": 6, "name": "1/8 sheet", "cost": 10, "display": "$10.00"},
                 {"id": 7, "name": "1/4 sheet", "cost": 15, "display": "$15.00"},
                 {"id": 8, "name": "1/2 sheet", "cost": 20, "display": "$20.00"},
-                {"id": 9, "name": "full sheet", "cost": 40, "display": "$40.00"}
+                {"id": 9, "name": "full sheet", "cost": 40, "display": "$40.00"},
+                {"id": 10, "name": "Other", "cost": 0, "display": "TBD"}
             ]
         };
         return LayerSizes;
@@ -113,7 +122,8 @@
                 {"id": 3, "name": 'Yellow', "cost": 0, "display": "0%"},
                 {"id": 4, "name": 'White', "cost": 0, "display": "0%"},
                 {"id": 5, "name": 'Spice', "cost": 0, "display": "0%"},
-                {"id": 6, "name": 'Carrot', "cost": 0, "display": "0%"}
+                {"id": 6, "name": 'Carrot', "cost": 0, "display": "0%"},
+                {"id": 7, "name": "Other", "cost": 0, "display": "TBD"}
             ]
         };
         return CakeFlavors;
@@ -128,10 +138,23 @@
                 {"id": 2, "name": 'Whipped', "cost": 0, "display": "0%"},
                 {"id": 3, "name": 'Chocolate Whipped', "cost": 0, "display": "0%"},
                 {"id": 4, "name": "Cream Cheese", "cost": 0.25, "display": "25%"},
-                {"id": 5, "name": "Fondant", "cost": 1, "display": "100%"}
+                {"id": 5, "name": "Fondant", "cost": 1, "display": "100%"},
+                {"id": 6, "name": "Other", "cost": 0, "display": "TBD"}
             ]
         };
         return FrostingFlavors;
+    }
+
+    function DecorationComplexity(){
+        var DecorationComplexity           = {};
+        DecorationComplexity.getComplexity = function(){
+            return [
+                {"id": 0, "name": 'Simple', "cost": 0, "display": "0%"},
+                {"id": 1, "name": 'Moderate', "cost": 0.25, "display": "25%"},
+                {"id": 2, "name": 'Complex', "cost": 0.50, "display": "50%"},
+            ]
+        };
+        return DecorationComplexity;
     }
 
     function StateFactory($http){
@@ -186,11 +209,12 @@
                     self[trackedItems[i]] = data[trackedItems[i]]
                 }
             },
-            totalup: function(size, cake, frosting){
-                data.total    = 0;
-                this.size     = size;
-                this.cake     = cake;
-                this.frosting = frosting;
+            totalup: function(size, cake, frosting, complexity){
+                data.total      = 0;
+                this.size       = size;
+                this.cake       = cake;
+                this.frosting   = frosting;
+                this.complexity = complexity;
                 for(var i = 0; i < data.layers.length; i++){
                     var currentLayer = data.layers[i];
                     for(var j = 0; j < layerElements.length; j++){
@@ -200,9 +224,14 @@
                                 // Size is the base price
                                 data.total += this[currentLayerElement][currentLayer[currentLayerElement]].cost;
                                 break;
-                            case 'frosting':
                             case 'cake':
+                            case 'frosting':
+                            case 'complexity':
                                 // These items are a percentage of the price of the layer's size
+                                console.log('currentLayerElement', currentLayerElement);
+                                console.log('currentLayer', currentLayer);
+                                console.log('currentLayer[currentLayerElement]', currentLayer[currentLayerElement]);
+                                console.log('this[currentLayerElement]', this[currentLayerElement]);
                                 data.total +=
                                     this.size[currentLayer.size].cost * this[currentLayerElement][currentLayer[currentLayerElement]].cost;
                                 break;
@@ -220,7 +249,7 @@
 
     function AppConfig($routeProvider, $locationProvider){
 
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
 
         $routeProvider
             .when('/', {
@@ -246,11 +275,12 @@
     }
 
     function OrderFormCtrl(OrderFormData, StateFactory, LayerElements){
-        var vm      = this;
-        vm.elements = LayerElements.getElements();
-        vm.cake     = LayerElements.CakeFlavors.getFlavors();
-        vm.frosting = LayerElements.FrostingFlavors.getFlavors();
-        vm.size     = LayerElements.LayerSizes.getSizes();
+        var vm        = this;
+        vm.elements   = LayerElements.getElements();
+        vm.size       = LayerElements.LayerSizes.getSizes();
+        vm.cake       = LayerElements.CakeFlavors.getFlavors();
+        vm.frosting   = LayerElements.FrostingFlavors.getFlavors();
+        vm.complexity = LayerElements.DecorationComplexity.getComplexity();
         StateFactory.getStates()
             .success(function(States){
                                  vm.states = States;
@@ -275,13 +305,14 @@
     }
 
     function PrintCtrl(OrderFormData, StateFactory, LayerElements){
-        var vm      = this;
-        vm.elements = LayerElements.getElements();
-        vm.cake     = LayerElements.CakeFlavors.getFlavors();
-        vm.frosting = LayerElements.FrostingFlavors.getFlavors();
-        vm.size     = LayerElements.LayerSizes.getSizes();
-        vm.total    = 0;
-        vm.isPayPal = function(){
+        var vm        = this;
+        vm.elements   = LayerElements.getElements();
+        vm.size       = LayerElements.LayerSizes.getSizes();
+        vm.cake       = LayerElements.CakeFlavors.getFlavors();
+        vm.frosting   = LayerElements.FrostingFlavors.getFlavors();
+        vm.complexity = LayerElements.DecorationComplexity.getComplexity();
+        vm.total      = 0;
+        vm.isPayPal   = function(){
             return vm.payment == 'PayPal';
         };
         StateFactory.getStates()
@@ -290,7 +321,7 @@
                                  if(!vm.state && vm.state !== 0)
                                      vm.state = 26;
                              });
-        OrderFormData.totalup(vm.size, vm.cake, vm.frosting);
+        OrderFormData.totalup(vm.size, vm.cake, vm.frosting, vm.complexity);
         OrderFormData.apply(vm);
     }
 })
